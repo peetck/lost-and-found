f1bd0f82cd669d5c857c3dcae18eba4dd2ba851d
peetck <peekung602@gmail.com>
2020-04-04 22:25:58 +0700

    Update :tada:

--- lost-and-found/posts/urls.py
+++ lost-and-found/posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('create/', views.create_view, name='create')
 ]
--- lost-and-found/posts/views.py
+++ lost-and-found/posts/views.py
@@ -1,27 +1,32 @@
 from django.shortcuts import render, HttpResponse
 from .forms import PostForm
 from .models import Post
-
+from django.views import View
 # Create your views here.
-def index_view(request):
-    posts = Post.objects.all()
-    losts = []
-    founds = []
-    active = 0
-    for post in posts:
-        if post.type == 'lost':
-            losts.append(post)
-        else:
-            founds.append(post)
-            if post.is_active:
-                active += 1
-    context = {
-        'losts' : losts,
-        'founds' : founds,
-        'active' : active,
-        'closed' : len(founds) - active
-    }
-    return render(request, 'index.html', context)
+
+class IndexView(View):
+    context = {}
+    template_name = 'index.html'
+    def get(self, request):
+        posts = Post.objects.all()
+        losts = []
+        founds = []
+        active = 0
+        for post in posts:
+            if post.type == 'lost':
+                losts.append(post)
+            else:
+                founds.append(post)
+                if post.is_active:
+                    active += 1
+
+        self.context['losts'] = losts
+        self.context['founds'] = founds
+        self.context['active'] = active
+        self.context['closed'] = len(founds) - active
+
+        return render(request, self.template_name, self.context)
+
 
 def create_view(request):
     if request.method == 'POST':
