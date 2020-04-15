9d88304b3235ae1fe5f9292f0eb21e058e98940d
peetck <peekung602@gmail.com>
2020-04-15 18:24:57 +0700

    Update (Chat Page) :tada:

--- lost-and-found/chats/urls.py
+++ lost-and-found/chats/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import ChatIndexView, ChatToView
+from .views import ChatIndexView
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', login_required(ChatIndexView.as_view()), name='chat_index'),
-    path('<int:user_id>/', login_required(ChatToView.as_view()), name='chat_to')
 ]
--- lost-and-found/chats/views.py
+++ lost-and-found/chats/views.py
@@ -1,51 +1,53 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views import View
 from .models import Message
 
+
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
+
 # Create your views here.
 
+@method_decorator(csrf_exempt, name='dispatch')
 class ChatIndexView(View):
     template_name = 'chat_index.html'
 
     def get(self, request):
-        users = User.objects.all().exclude(id=request.user.id)
+        return render(request, self.template_name, {})
 
-        context = {
-            'users' : users
-        }
-        return render(request, self.template_name, context)
+    def post(self, request):
+        users = User.objects.all()
+        data = []
+        for user in users:
+            if user == request.user:
+                continue
+            sends = user.chat.message_set.all()
+            gets = Message.objects.filter(to=user)
 
-class ChatToView(View):
-    template_name = 'chat_to.html'
+            if len(sends) != 0 and len(gets) != 0:
+                last_send_msg = sends.order_by('-timestamp')[0]
+                last_get_msg = gets.order_by('-timestamp')[0]
+                if last_send_msg.timestamp > last_get_msg.timestamp:
+                    message = last_send_msg.message
+                    datetime = last_send_msg.timestamp
+                else:
+                    message = last_get_msg.message
+                    datetime = last_get_msg.timestamp
+            elif len(sends) != 0:
+                last_send_msg = sends.order_by('-timestamp')[0]
+                message = last_send_msg.message
+                datetime = last_send_msg.timestamp
+            elif len(gets) != 0:
+                last_get_msg = gets.order_by('-timestamp')[0]
+                message = last_get_msg.message
+                datetime = last_get_msg.timestamp
+            else:
+                message = ''
+                datetime = ''
+            data.append([user.username, message, datetime, user.userprofile.avatar.url, user.id])
 
-    def get(self, request, user_id):
-        user = User.objects.get(id=user_id)
-
-        send_messages = request.user.chat.message_set.all()
-        get_messages = user.chat.message_set.all()
-        return render(request, self.template_name, {
-            'user' : user,
-            'send_messages' : send_messages,
-            'get_messages' : get_messages
-        })
-
-    def post(self, request, user_id):
-
-        user = User.objects.get(id=user_id)
-
-        Message.objects.create(
-            message=request.POST.get('msg'),
-            seen=False,
-            to=user,
-            chat=request.user.chat
-        )
-
-        send_messages = request.user.chat.message_set.all()
-        get_messages = user.chat.message_set.all()
-
-        return render(request, self.template_name, {
-            'user' : user,
-            'send_messages' : send_messages,
-            'get_messages' : get_messages
-        })
+        return JsonResponse({'users' : data})
