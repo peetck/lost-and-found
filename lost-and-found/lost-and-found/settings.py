581752789ac2cc9ec1771141498ae4a39a895433
peetck <peekung602@gmail.com>
2020-04-29 02:30:48 +0700

    Update settings.py

--- lost-and-found/lost-and-found/settings.py
+++ lost-and-found/lost-and-found/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h3mygx(^*!g=pb8m)hmg)4v0a3plzwzats154&0pfgev7%kpx('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://lost-found.azurewebsites.net']
 
 
 # Application definition
